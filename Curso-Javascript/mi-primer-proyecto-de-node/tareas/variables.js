145c6bc7f2f7cbadfca3686303e2f3d5fb30eefa
jhamil06 <128666918+jhamil06@users.noreply.github.com>
2023-07-21 17:26:01 -0400

    tarea

---
+++ Curso-Javascript/mi-primer-proyecto-de-node/tareas/variables.js
@@ -0,0 +1,11 @@
+const name = "Jhamil Kevin Huanca Mejia"
+const edad = 23
+const desarrollador= true
+const nacimiento = date(1999, 12, 6)
+const libro={
+     titulo : "Divina comedia",
+     autor : "dante aligueri",
+     fecha : undefined,
+    url : undefined
+
+};
